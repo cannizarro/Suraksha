a2dc2e78201cb50ee0f591fa558dc56806df624a
cannizarro <asrarul97@gmail.com>
2020-04-09 00:50:23 +0530

    Minor code fixes (Deleted the onDestoy)

--- app/src/main/java/com/cannizarro/securitycamera/CameraActivity.java
+++ app/src/main/java/com/cannizarro/securitycamera/CameraActivity.java
@@ -66,6 +66,11 @@ public class CameraActivity extends AppCompatActivity {
     final String TAG = "CameraActivity";
     final int ALL_PERMISSIONS_CODE = 1;
     static final int MEDIA_TYPE_VIDEO = 1;
+    String permissions[] = {Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     private boolean isRecording = false, isScreenOn=true, isInitiator=false, isStarted=false;
     private String username, cameraName;
     PeerConnection localPeer;
@@ -147,12 +152,11 @@ public class CameraActivity extends AppCompatActivity {
         });
 
 
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, ALL_PERMISSIONS_CODE);
+        if (ContextCompat.checkSelfPermission(this, permissions[0]) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, permissions[1]) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, permissions[2]) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, permissions[3]) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions, ALL_PERMISSIONS_CODE);
         } else {
             // all permissions already granted
             start();
@@ -181,10 +185,23 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if(isRecording)
-            controlRecording();
+
         if(isStarted)
             hangup();
+        if(isRecording)
+            controlRecording();
+
+        if(videoCapturerAndroid != null){
+            try {
+                videoCapturerAndroid.stopCapture();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            surfaceTextureHelper.stopListening();
+            surfaceTextureHelper.dispose();
+            localVideoView.release();
+        }
+
     }
 
 
@@ -441,26 +458,6 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onDestroy() {
-        if(isStarted)
-            hangup();
-        if(isRecording)
-            controlRecording();
-
-        if(videoCapturerAndroid != null){
-            try {
-                videoCapturerAndroid.stopCapture();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            surfaceTextureHelper.stopListening();
-            surfaceTextureHelper.dispose();
-            localVideoView.release();
-        }
-
-        super.onDestroy();
-    }
 
     /**
      * Pushing ice candidates to firebase.
